e371b3933581b93f6abe6ba7f64e001694f1b642
Werenskjold <Rilliden@gmail.com>
2019-10-20 20:54:16 +0900

    Cleaned up some code
    Got internet out here in the 田舎!
    
    Cleaned up and refactored the code a bit.

--- main.cpp
+++ main.cpp
@@ -1,15 +1,25 @@
+/*
+	Project Name: SNES-Style Driving Prototype
+	Dev: Hunter Werenskjold
+	Date: 10/1/19
+
+	Description:
+		Out of curiosity, I wanted to see what would go into making a psuedo-3D real-time renderer for a driving game.
+*/
+
 #include <iostream>
 #include <vector>
+#include <time.h>
 
 #include "gfxHelper.h"
 
-#define WINDOW_WIDTH 600
-#define WINDOW_HEIGHT 600
+#define WINDOW_WIDTH 400
+#define WINDOW_HEIGHT 400
 #define WINDOW_PIXELS WINDOW_HEIGHT * WINDOW_WIDTH
 
-#define DRAW_DISTANCE 250
-#define SEGMENTS DRAW_DISTANCE
-#define SEGMENT_WIDTH 100
+#define SEGMENTS 50
+#define SEGMENT_WIDTH 20
+#define PERCENT_RUNNER 0.10f
 
 #define DELTA_TIME 1/60.f
 #define DEFAULT_R 140
@@ -17,11 +27,11 @@
 #define DEFAULT_B 220
 
 #define CAMERA_HEIGHT 100
-#define CAMERA_FOV 45
+#define CAMERA_FOV 90
 
 #define ROAD_WIDTH_DEFAULT 200.f
 
-#define MAX_SPEED_VERTICAL 4.f
+#define MAX_SPEED_VERTICAL 1.f
 #define MAX_SPEED_HORIZONTAL 0.2f
 
 enum ObjectShape
@@ -52,22 +62,27 @@ static struct Camera {
 } camera;
 
 struct Segment {
-	Segment(Vector2D tr, Vector2D tl, Vector2D br, Vector2D bl, float c) : topRight{ tr }, topLeft{tl}, bottomRight{ br }, bottomLeft{ bl }, color{ c }
+	Segment(Vector2D tr, Vector2D tl, Vector2D br, Vector2D bl, float c, bool t) : topRight{ tr }, topLeft{tl}, bottomRight{ br }, bottomLeft{ bl }, color{ c }, type{ t }
 	{	}
 
 	// We will track the top-left points and the center of the top ( for simplicity sake )
 	Vector2D topRight, bottomRight, topLeft, bottomLeft;
 	float color = 0;
+	bool type = 0;
 };
 
 SDL_Window* window;
 SDL_Renderer* renderer;
 
 std::vector<Segment*> segments;
+int segmentsPassed = 0;
 
 bool keys[5]; // 0 - up, 1 - left, 2 - down, 3 - right, 4 - space
 
 float iTime = 0.f;
+
+// Testing-related
+clock_t cycleTimeStart = 0.f, cycleTimeEnd = 0.f;
 
 bool isRunning = true;
 
@@ -77,6 +92,7 @@ void draw();
 
 void initialize();
 
+// This is the bulk of how to render in this style
 void project(Vector2D& v)
 {
 	// Transform the point to camera space
@@ -84,10 +100,10 @@ void project(Vector2D& v)
 	float cX = v.x - camera.v.x, cY = v.y - camera.v.y, cZ = v.z - camera.v.z;
 	float scaling = camera.depth / cZ;
 
-	// Transform the point from camera space x:[-1 : 1] and y:[-1 : 1] to screen space x:[0 : 1] y:[0 : 1] from the top-left to the bottom-right and apply proper scaling
+	// Transform the point from camera space to screen space x:[0 : 1] y:[0 : 1] from the top-left to the bottom-right and apply proper scaling
 	v.sX = round((WINDOW_WIDTH / 2.f) + (scaling * cX * WINDOW_WIDTH / 2.f));
 	v.sY = round((WINDOW_HEIGHT / 2.f) - (scaling * cY * WINDOW_HEIGHT / 2.f));
-	v.sZ = round((scaling * ROAD_WIDTH_DEFAULT * WINDOW_WIDTH / 2.f));
+	v.sZ = round((scaling * ROAD_WIDTH_DEFAULT * (float)WINDOW_WIDTH / 2.f));
 }
 
 void drawSegment(Segment& s)
@@ -99,7 +115,7 @@ void drawSegment(Segment& s)
 	project(s.topLeft);
 	project(s.topRight);
 
-	if (s.topLeft.z - camera.v.z < 0)
+	if (s.topLeft.z - camera.v.z - (camera.depth * camera.v.y) < 0)
 		return;
 
 	int bottomRow, topRow;
@@ -112,26 +128,38 @@ void drawSegment(Segment& s)
 	if (rows <= 0)
 		return;
 
-	float leftSlope = (s.topLeft.sY - s.bottomLeft.sY)/(s.topLeft.sX - s.bottomLeft.sX);
-	float rightSlope = (s.topRight.sY - s.bottomRight.sY)/(s.topRight.sX - s.bottomRight.sX);
+	float leftSlope = 1.f/((s.topLeft.sY - s.bottomLeft.sY)/(s.topLeft.sX - s.bottomLeft.sX));
+	float rightSlope = 1.f/((s.topRight.sY - s.bottomRight.sY)/(s.topRight.sX - s.bottomRight.sX));
 
-	/*
-	For each row r; iterate from p1 to p2
-		draw road w/ color and width calculated from topWidth + i * interpolationValue
-	*/
+	int tLmBl = s.topLeft.sY - s.bottomLeft.sY;
+	int tRmBr = s.topRight.sY - s.bottomRight.sY;
+
 	for (int i = 0; i < rows; i++)
 	{
 		// Draw ground
 		SDL_SetRenderDrawColor(renderer, 230, 200, 144, 255);
 		SDL_RenderDrawLine(renderer, 0, s.topLeft.sY + i, WINDOW_WIDTH, s.topLeft.sY + i);
 
-		// Calculate slope-intercept form for x
-		int x1 = ((s.topLeft.sY + i) - s.bottomLeft.sY) / leftSlope;
-		int x2 = ((s.topRight.sY + i) - s.bottomRight.sY) / rightSlope;
+		// Calculate x value given y-position
+		int x1 = (i + tLmBl) * leftSlope;
+		int x2 = (i + tRmBr) * rightSlope;
+
+		// Calculate inner points from x1 and x2
+		int runnerLength = fabsf((s.bottomLeft.sX + x1) - (s.bottomRight.sX + x2)) * PERCENT_RUNNER;
 
 		// Draw Road
+		// Draw left edge, center, right edge
+
+		if (s.type == 0)
+			SDL_SetRenderDrawColor(renderer, 200, 100, 100, 255);
+		else
+			SDL_SetRenderDrawColor(renderer, 240, 240, 240, 255);
+		SDL_RenderDrawLine(renderer, s.bottomLeft.sX + x1, s.topLeft.sY + i, s.bottomLeft.sX + x1 + runnerLength, s.topRight.sY + i);
+		SDL_RenderDrawLine(renderer, s.bottomRight.sX + x2, s.topLeft.sY + i, s.bottomRight.sX + x2 - runnerLength, s.topRight.sY + i);
+
+
 		SDL_SetRenderDrawColor(renderer, s.color, s.color, s.color, 255);
-		SDL_RenderDrawLine(renderer, s.bottomLeft.sX + x1, s.topLeft.sY + i, s.bottomRight.sX + x2, s.topRight.sY + i);
+		SDL_RenderDrawLine(renderer, s.bottomLeft.sX + x1 + runnerLength, s.topLeft.sY + i, s.bottomRight.sX + x2 - runnerLength, s.topRight.sY + i);
 	}
 
 }
@@ -232,9 +260,43 @@ void updateInputs(SDL_Event& e)
 			keys[4] = 0;
 }
 
-void update()
+// Check clipping of objects and segments
+void checkClipping()
 {
+	// Clip Objects
+	/// ... Kinda similar to the above check, just do it with an array of objects
+}
 
+// We also handle clipping here
+void segmentGeneration()
+{
+	/// Check the closest segment for clipping. If we should clip it, then we will remove the pointer from the vector alter the data accordingly and push it back on
+	/// This should allow us to render only what we need, store only what we need, etc.
+	if ((*segments[0]).topLeft.z - player.v.z < 0)
+	{
+		segments.erase(segments.begin());
+
+		Uint8 color = 200;
+		bool type = 0;
+		if ((*segments[SEGMENTS - 1]).color == 200)
+		{
+			color = 220;
+			type = 1;
+		}
+
+		// Generate and push back new segment onto vector
+		// Preferably, we should probably just recycle the erased segment's data
+		segments.push_back(new Segment(
+			Vector2D(ROAD_WIDTH_DEFAULT / 2.f, 0, (*segments[SEGMENTS - 1]).topLeft.z + SEGMENT_WIDTH),
+			Vector2D(-1 * ROAD_WIDTH_DEFAULT / 2.f, 0, (*segments[SEGMENTS - 1]).topRight.z + SEGMENT_WIDTH),
+			Vector2D(ROAD_WIDTH_DEFAULT / 2.f, 0, (*segments[SEGMENTS - 1]).topLeft.z),
+			Vector2D(-1 * ROAD_WIDTH_DEFAULT / 2.f, 0, (*segments[SEGMENTS - 1]).topRight.z),
+			color, type));
+	}
+}
+
+void input()
+{
 	if (keys[0] == 1 && player.vertSpeed < MAX_SPEED_VERTICAL)
 	{
 		player.vertSpeed += 0.001f;
@@ -260,6 +322,17 @@ void update()
 
 	camera.v.z = player.v.z - camera.depth * camera.v.y;
 	camera.v.x = player.v.x;
+}
+
+void update()
+{
+
+	// Object Clipping
+	checkClipping();
+	segmentGeneration();
+
+	// Input
+	input();	
 	
 }
 
@@ -269,8 +342,9 @@ void draw()
 	SDL_RenderClear(renderer);
 
 	project(player.v);
-
-	for (int i = 1; i < SEGMENTS; i++)
+	
+	// Draw each segment
+	for (int i = 0; i < SEGMENTS; i++)
 	{
 		drawSegment(*segments[i]);
 	}
@@ -278,8 +352,10 @@ void draw()
 	Vector2D pd(0, 0, 1000);
 	project(pd);
 
-	SDL_SetRenderDrawColor(renderer, 144, 255, 144, 255);
-	gfxDrawBrenCircle(renderer, pd.sX, pd.sY, 10.f, true);
+	if (pd.z > player.v.z) {
+		SDL_SetRenderDrawColor(renderer, 144, 255, 144, 255);
+		gfxDrawBrenCircle(renderer, pd.sX, pd.sY, 10, true);
+	}
 
 	SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
 	gfxDrawBrenCircle(renderer, player.v.sX, player.v.sY, 20.f, true);
@@ -300,13 +376,21 @@ void initialize()
 	player.v.z = camera.depth*camera.v.y;
 
 	// Going closest to farthest
-	for (int i = 0; i <= DRAW_DISTANCE; i++)
+	for (int i = 0; i <= SEGMENTS; i++)
 	{
+		Uint8 color = 200;
+		bool type = 0;
+		if (i % 2 == 0)
+		{
+			color = 220;
+			 type = 1;
+		}
+
 		segments.push_back(new Segment(
 			Vector2D(ROAD_WIDTH_DEFAULT/2.f, 0, SEGMENT_WIDTH + i * SEGMENT_WIDTH),
 			Vector2D(-1 * ROAD_WIDTH_DEFAULT / 2.f, 0, SEGMENT_WIDTH + i * SEGMENT_WIDTH),
 			Vector2D(ROAD_WIDTH_DEFAULT/2.f, 0, i * SEGMENT_WIDTH),
 			Vector2D(-1 * ROAD_WIDTH_DEFAULT/2.f, 0, i * SEGMENT_WIDTH),
-			244 * (i % 2)));
+			color, type));
 	}
 }
