cf4eae463937556bc062327eeda10fcf71bd1a89
Werenskjold <Rilliden@gmail.com>
2020-07-14 12:06:39 +0900

    Pressing Esc Now Closes Game

--- main.cpp
+++ main.cpp
@@ -58,7 +58,7 @@ int main(int args, char* argv[])
 	{
 		while (SDL_PollEvent(&e))
 		{
-			if (e.type == SDL_QUIT)
+			if (e.type == SDL_QUIT || (e.type == SDL_KEYDOWN && e.key.keysym.sym == SDLK_ESCAPE))
 			{
 				isRunning = false;
 			}
