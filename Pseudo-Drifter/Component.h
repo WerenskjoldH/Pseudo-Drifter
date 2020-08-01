8aa540b7ca954ba7e4293f403c8c472710cecf16
Werenskjold <Rilliden@gmail.com>
2020-08-01 17:40:43 +0900

    Created Entity, Component, And Drawable Component Foundation Classes

---
+++ Pseudo-Drifter/Component.h
@@ -0,0 +1,15 @@
+#ifndef COMPONENT_H
+#define COMPONENT_H
+	
+/*
+	Description: This file contains the component class declaration
+
+	This class is a safe-guard for future extension of functionality and grouping, however, for now it just exists to be inherited from
+*/
+
+class Component
+{
+
+};
+
+#endif
